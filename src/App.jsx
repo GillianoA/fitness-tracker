f4ff86ef8b35e2fe1f066b1ce38e720ae4b979df
GiillianoA <agard.gilliano@gmail.com>
2025-02-05 15:51:26 -0400

    Intitial project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="w-screen h-screen flex items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold underline">Hello, Tailwind!</h1>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
