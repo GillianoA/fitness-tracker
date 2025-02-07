d423f0d0d957c70f067898fd44de413d2a985590
GiillianoA <agard.gilliano@gmail.com>
2025-02-07 17:09:29 -0400

    Fixed text wrapping issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                 <Link to="/" className="text-gray-400 hover:text-white mx-2 flex items-center gap-2 text-inherit no-underline" >
                 <Dumbbell className="w-5 h-5" />Dashboard
                 </Link>
-                <Link to="/log" className="text-gray-400 hover:text-white mx-2 flex items-center gap-2 text-inherit no-underline">
+                <Link to="/log" className="text-gray-400 hover:text-white mx-2 flex items-center gap-2 text-inherit no-underline whitespace-nowrap">
                 <ClipboardList className="w-5 h-5" />Workout Log
                 </Link>
                 <Link to="/progress" className="text-gray-400 hover:text-white mx-2 flex items-center gap-2 text-inherit no-underline">
